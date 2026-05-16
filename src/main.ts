bb6cd3ab873df6db989b665bab7f3e5eb2526c2c
malaz22mm <malazmalaz71@gmail.com>
2026-05-16 09:39:03 +0300

    s commit

--- src/main.ts
+++ src/main.ts
@@ -41,7 +41,11 @@ async function bootstrap() {
       'https://hrdashboardai.netlify.app',
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: '*',
+    allowedHeaders: [  'Content-Type',
+      'Authorization',
+      'Accept',
+      'Origin',
+      'X-Requested-With',],
     credentials: true,
   });
 
